ec0a85307193c85c5baed4c6cf9f1afb8c2f6ed0
kalczugag <kalczugag@gmail.com>
2022-10-05 14:04:03 +0200

    code format

--- src/main.ts
+++ src/main.ts
@@ -3,47 +3,53 @@ let passwdLength = document.getElementById("passwdLength") as HTMLDivElement;
 const copyBtn: any = document.getElementById("copy");
 
 const copyToClipboard = () => {
-  if (outputPass.innerHTML === null) {
-    return;
-  } else {
-    const elem = document.createElement("textarea");
-    elem.value = outputPass.innerHTML;
-    document.body.appendChild(elem);
-    elem.select();
-    document.execCommand("copy");
-    document.body.removeChild(elem);
-  }
+    if (outputPass.innerHTML === null) {
+        return;
+    } else {
+        const elem = document.createElement("textarea");
+        elem.value = outputPass.innerHTML;
+        document.body.appendChild(elem);
+        elem.select();
+        document.execCommand("copy");
+        document.body.removeChild(elem);
+    }
 };
 
 passwdLength.innerHTML = "32";
 const getSliderValue = (value: any) => {
-  passwdLength.innerHTML = value;
+    passwdLength.innerHTML = value;
 };
 
 let passwordSignsArray: string[] = [];
 
 const passwordSettings = () => {
-  if ((document.getElementById("lowerCase") as HTMLInputElement).checked) {
-    passwordSignsArray.concat(LOWERCASE);
-  } else if (
-    (document.getElementById("upperCase") as HTMLInputElement).checked
-  ) {
-    passwordSignsArray.concat(UPPERCASE);
-  } else if ((document.getElementById("numbers") as HTMLInputElement).checked) {
-    passwordSignsArray.concat(NUMBERS);
-  } else if ((document.getElementById("symbols") as HTMLInputElement).checked) {
-    passwordSignsArray.concat(SYMBOLS);
-  } else if (
-    (document.getElementById("duplicate") as HTMLInputElement).checked
-  ) {
-    console.log("xd");
-  } else if ((document.getElementById("spaces") as HTMLInputElement).checked) {
-    console.log("xd");
-  }
+    if ((document.getElementById("lowerCase") as HTMLInputElement).checked) {
+        passwordSignsArray.concat(LOWERCASE);
+    } else if (
+        (document.getElementById("upperCase") as HTMLInputElement).checked
+    ) {
+        passwordSignsArray.concat(UPPERCASE);
+    } else if (
+        (document.getElementById("numbers") as HTMLInputElement).checked
+    ) {
+        passwordSignsArray.concat(NUMBERS);
+    } else if (
+        (document.getElementById("symbols") as HTMLInputElement).checked
+    ) {
+        passwordSignsArray.concat(SYMBOLS);
+    } else if (
+        (document.getElementById("duplicate") as HTMLInputElement).checked
+    ) {
+        console.log("xd");
+    } else if (
+        (document.getElementById("spaces") as HTMLInputElement).checked
+    ) {
+        console.log("xd");
+    }
 };
 
 passwordSettings();
 
 function showTable() {
-  console.log(passwordSignsArray);
+    console.log(passwordSignsArray);
 }
